ef5ef52b3ff290e451de554db45e908ee2f90ce0
Lee Turner <lee@cmune.com>
2013-07-17 18:09:45 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+A simple Chinese dictionary using cc-cedict.
+
+Can run it with a word (i.e. cedict word) to get definitions for that word, or run it without parameters to get an interactive console.
---
+++ cedict.js
@@ -0,0 +1,115 @@
+var fs = require('fs');
+var colours = require('colours');
+var sqlite3 = require('sqlite3').verbose();
+var readline = require('readline');
+var http = require('http');
+var zlib = require('zlib');
+var replMode = false;
+
+var rl = readline.createInterface({
+  input: process.stdin,
+    output: process.stdout
+});
+
+
+if(!fs.existsSync("cedict.sqlite3")) {
+  var reqData = "";
+  var unzippedData;
+  var request = http.get("http://www.mdbg.net/chindict/export/cedict/cedict_1_0_ts_utf-8_mdbg.txt.gz");
+  request.on('data', function() {
+    process.stdout.write(".");
+  });
+  request.on('error', function() {
+    console.log("Error downloading dictionary");
+  });
+  request.on('response', function(response) {
+    var output = fs.createWriteStream('cedict_ts.u8');
+    response.pipe(zlib.createGunzip()).pipe(output);
+    response.on('end', function() {
+      buildDB();
+    });
+  });
+  console.log("Downloading dictionary, please standby");
+} else if (process.argv.length > 2)  {
+  findDefinitions(process.argv[2]);
+} else {
+  repl();
+}
+
+function buildDB() {
+  var newDb = new sqlite3.Database("cedict.sqlite3");
+  var fileContents = fs.readFileSync('cedict_ts.u8');
+  var entries = fileContents.toString().split('\n');
+  console.log("Building database");
+  
+  // actually, using a regex is a bad idea 
+  // http://eli.thegreenplace.net/2013/07/16/hand-written-lexer-in-javascript-compared-to-the-regex-based-ones/
+  // but I found this regex here https://github.com/Tropi/php-cedict/blob/master/cedict2mysql.php
+  // and I really don't want to spend too long getting a working version of this up and running
+  var regex = /(.*?) (.*?) \[(.*?)\] \/(.*)\//;
+  newDb.run("CREATE TABLE dict (id  INTEGER PRIMARY KEY AUTOINCREMENT, traditional VARCHAR(50), simplified  VARCHAR(50), pinyin      VARCHAR(100), english     VARCHAR(500) )"); 
+  newDb.serialize(function() {
+    var transact = newDb.prepare("INSERT INTO dict(traditional, simplified, pinyin, english) VALUES(?, ?, ?, ?)");
+    for(var index in entries) {
+      if(entries[index].indexOf("#") != 0) {
+        var parsedEntry = entries[index].match(regex);
+        if(parsedEntry != null) {
+          transact.run(parsedEntry[1], parsedEntry[2], parsedEntry[3], parsedEntry[4]);
+        }
+      }
+    } 
+    transact.finalize(function() {
+      process.stdout.write("\nDone!");
+      newDb.close();
+      fs.unlink('cedict_ts.u8');
+      repl();
+    });
+  });
+}
+
+function repl() {
+  replMode = true;
+  rl.setPrompt("=> ");
+  rl.prompt(true);
+
+  rl.on('line', function(line) {
+    findDefinitions(line);
+  }); 
+}
+
+function findDefinitions(line) {
+  var db = new sqlite3.Database("cedict.sqlite3");
+  var selector = line + "%";
+  db.each("SELECT traditional, simplified, pinyin, english FROM dict WHERE traditional LIKE ? OR simplified LIKE ? OR pinyin LIKE ? or english LIKE ? LIMIT 10", selector, selector, selector, "%" + selector, function(err, entry) {
+    if(err) return;
+    console.log();
+    console.log(entry["simplified"] + " [ ".bold + entry["traditional"] + " ]".bold);
+    var pinyin = entry["pinyin"].split(' ');
+    var outputPinyin = "";
+    for(var i in pinyin) {
+      if(pinyin[i].indexOf("1") != -1) { 
+        outputPinyin += pinyin[i].red + " "; 
+      } else if (pinyin[i].indexOf("2") != -1) { 
+        outputPinyin += pinyin[i].yellow + " "; 
+      } else if(pinyin[i].indexOf("3") != -1) { 
+        outputPinyin += pinyin[i].green + " "; 
+      } else if(pinyin[i].indexOf("4") != -1) { 
+        outputPinyin += pinyin[i].cyan + " "; 
+      } else {
+        outputPinyin += pinyin[i];
+      }
+    }
+    console.log(outputPinyin);
+    var english = entry["english"].split('/');
+    for(var i in english) {
+      console.log((parseInt(i) + 1) + ". " + english[i]);
+    }
+  }, function() {
+    if(replMode) {
+      rl.prompt(true);
+    } else {
+      db.close();
+      process.exit();
+    }
+  });
+}
